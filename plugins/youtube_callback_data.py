b1d38f81b576bc214aa76a1657083c99bcc5737d
amansharma999 <aman.gamestar999@gmail.com>
2021-04-09 01:22:48 +0530

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-
+import time
 from pyrogram import (Client,
                       InlineKeyboardButton,
                       InlineKeyboardMarkup,
@@ -30,7 +30,9 @@ async def catch_youtube_fmtid(c, m):
         else:
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
                 "Video", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
-                                                                                                    callback_data=f"docvideo||{format_id}||{yturl}")]])
+        print('sleeping for a sec')                                                                                           callback_data=f"docvideo||{format_id}||{yturl}")]])
+        time.sleep(1)
+
 
         await m.edit_message_reply_markup(buttons)
 
